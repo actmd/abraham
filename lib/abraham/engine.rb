e84ba2b4e4ea6a29432c231e6ba4be37c23a305f
Jonathan Abbett <jonathan@act.md>
2016-12-20 10:54:57 -0500

    change dependencies

--- lib/abraham/engine.rb
+++ lib/abraham/engine.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 require 'rubygems'
-require 'rails-assets-shepherd.js'
-require 'jquery-rails'
-require 'rails-assets-js-cookie'
 
 module Abraham
   class Engine < ::Rails::Engine
+    require 'rails-assets-shepherd.js'
+    require 'jquery-rails'
+    require 'rails-assets-js-cookie'
   end
 end
