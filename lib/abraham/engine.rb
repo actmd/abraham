6d91ce078889aa1d36da4ae633301733d3244a5a
Jonathan Abbett <jonathan@act.md>
2018-05-08 14:23:55 -0400

    Remove requires from engine

--- lib/abraham/engine.rb
+++ lib/abraham/engine.rb
@@ -3,8 +3,5 @@ require 'rubygems'
 
 module Abraham
   class Engine < ::Rails::Engine
-    require 'rails-assets-shepherd.js'
-    require 'jquery-rails'
-    require 'rails-assets-js-cookie'
   end
 end
