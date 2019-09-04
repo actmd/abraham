acefeb5f95c540fbea3295334a2510110a7ed5e1
Jonathan Abbett <jonathan@act.md>
2019-09-04 12:37:23 -0400

    Set up automated testing for Rails 5.1, 5.2, and 6.0 with TravisCI

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -16,5 +16,5 @@ Capybara.register_driver(:headless_chrome) do |app|
 end
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
 end
