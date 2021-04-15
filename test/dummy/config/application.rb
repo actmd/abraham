71e201feab86a605290e4b08b484059a94ea2169
Jonathan Abbett <jonathan@act.md>
2021-04-15 14:37:56 -0400

    Fix to rails version check

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -13,8 +13,8 @@ module Dummy
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # This configuration is no longer necessary with Rails 6.1+
-    unless Rails::VERSION::MAJOR >= 6 && Rails::VERSION::MINOR >= 1
+    # For Rails 5.2 - 6.0, we need to set this configuration
+    if (Rails::VERSION::MAJOR == 5 && Rails::VERSION::MINOR > 1) || (Rails::VERSION::MAJOR == 6 && Rails::VERSION::MINOR < 2)
       config.active_record.sqlite3.represent_boolean_as_integer = true
     end
   end
