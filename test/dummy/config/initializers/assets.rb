b8cb255667e2564e71428184d7316ee2fec46007
Aashish Saini <asaini65@gmail.com>
2023-10-25 21:21:34 +0530

    Move dependencies from node modules to assets

--- test/dummy/config/initializers/assets.rb
+++ test/dummy/config/initializers/assets.rb
@@ -9,7 +9,7 @@ Rails.application.config.assets.version = "1.0"
 # Rails.application.config.assets.paths << Emoji.images_path
 
 # Add Yarn node_modules folder to the asset load path.
-Rails.application.config.assets.paths << Rails.root.join('../../node_modules')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'javascripts')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
